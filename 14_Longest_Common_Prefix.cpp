2df3d3d7214ee43c784888d9dd3b1f57d2716304
Vedant11054 <vedantghubade04@gmail.com>
2026-03-28 20:58:55 +0530

    longest common prefix final commit -F

--- 14_Longest_Common_Prefix.cpp
+++ 14_Longest_Common_Prefix.cpp
@@ -2,13 +2,13 @@
 #include<vector>
 using namespace std;
 
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
+string strings() 
+{
+        string strs[]={"flights","flour","flower","flight"};
         string ans = "";
         for (int i = 0; i < strs[0].length(); i++) 
         {
-            for (int j = 0; j < strs.size() - 1; j++) 
+            for (int j = 0; j < strs[i].size() - 1; j++) 
             {
                 if (strs[j][i] != strs[j + 1][i]) 
                 {
@@ -18,5 +18,9 @@ public:
             ans += strs[0][i];
         }
         return ans;
-    }
-};
+}
+
+int main()
+{
+    cout<< strings;
+}
