dec66dc6f27df516a9c92502eaabe6a5c6bd436a
Vedant11054 <vedantghubade04@gmail.com>
2026-04-08 09:49:33 +0530

    27 remove element from a list other approach -C

--- 27_remove_element.cpp
+++ 27_remove_element.cpp
@@ -20,22 +20,32 @@ public:
 
 int main()
 {
-    vector<int>nums={3,2,2,3};
-    int val=3;
-    vector<int>nums1;
+//     vector<int>nums={3,2,2,3};
+//     int val=3;
+//     vector<int>nums1;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i]!=val)
+//             {
+//                 nums1.push_back(nums[i]);
+//             }
+//         }
+//         for (int i = 0; i < nums1.size(); i++)
+//         {
+//             /* code */
+//             cout<<nums1[i];
+//         }
+//         cout<<endl;
+//         cout<<nums1.size();
+        int k=0;
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i]!=val)
             {
-                nums1.push_back(nums[i]);
+                nums[k]=nums[i];
+                k++;
             }
         }
-        for (int i = 0; i < nums1.size(); i++)
-        {
-            /* code */
-            cout<<nums1[i];
-        }
-        cout<<endl;
-        cout<<nums1.size();
+            return k;
         
 }
